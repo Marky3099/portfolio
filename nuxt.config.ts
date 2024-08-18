3232bdc4043f4927ab66e29fb9f663fdda38596c
Manabat, Markrey B. <92512534+Marky3099@users.noreply.github.com>
2024-08-18 19:23:31 +0800

    Settings for Deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,30 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/index.css"],
+  nitro:{
+    prerender:{
+      autoSubfolderIndex: false,
+    }
+  },
+  app:{
+    head:{
+      title:"Markrey's Portfolio",
+      meta: [
+        {charset: "utf-8"},
+        {
+          name:"viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          hid:"description",
+          name:"description",
+          content:"Marky's Portfolio",
+        },
+        {
+          name:"keywords",
+          content:"portfolio,website,dev,developer,programmer"
+        }
+      ]
+    }
+  }
 })
